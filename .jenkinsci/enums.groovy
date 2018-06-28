6f8670ddec8037f34126080efbf235c4ebf0097a
tyvision <antykushin@gmail.com>
2018-06-28 10:54:56 +0300

    test
    Signed-off-by: tyvision <antykushin@gmail.com>

--- .jenkinsci/enums.groovy
+++ .jenkinsci/enums.groovy
@@ -1,7 +1,6 @@
 // types of tests provided by the developers (can be found at the CMakeLists.txt files)
 enum TestTypes {
 	module, integration, system, cmake, regression, benchmark, framework
-	public TestTypes() {}
 }
 
 return this
